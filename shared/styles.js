518a3238d1f8afc1ce391903d2d260ce730172d7
Matthias Gattermeier <matthias.gattermeier@nytimes.com>
2016-01-15 11:51:17 -0500

    initial commit

---
+++ shared/styles.js
@@ -0,0 +1,18 @@
+module.exports = {
+  LazyImage: {
+    true: {
+      opacity: 0,
+    },
+    false: {
+      opacity: 1,
+    }
+  },
+  FullImage: {
+    true: {
+      opacity: 1,
+    },
+    false: {
+      opacity: 0,
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,106 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const StackBlur = require('../shared/stackblur');
+
+function getStyles(target, styles, condition) {
+  let conditionalStyles = require('../shared/styles');
+
+  return Object.assign({},
+    styles,
+    condition && conditionalStyles[target].true,
+    !condition && conditionalStyles[target].false
+  )
+}
+
+function getClass(target, condition) {
+  return condition && 'seemslegit' || 'ups'
+}
+
+class FullImage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    var {src, onLoad, style, ...other} = this.props
+    return (
+      <div style={style}>
+        <img src={src} style={style} onLoad={onLoad} />
+      </div>
+    )
+  }
+}
+class LazyImage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount(){
+    var container = ReactDOM.findDOMNode(this);
+    this.width = container.offsetWidth;
+    this.height = container.offsetHeight;
+    this.canvas = ReactDOM.findDOMNode(this.refs.canvas);
+    this.canvas.height = this.height;
+    this.canvas.width = this.width;
+    var blurRadius = this.props.blurRadius || 0;
+    this.preImg = document.createElement('img');
+    this.preImg.crossOrigin = 'Anonymous';
+    this.preImg.onload = () => {
+      StackBlur(this.preImg, this.canvas, blurRadius, 600, 190);
+    };
+    this.preImg.src = this.props.src;
+  }
+  componentWillUpdate(nextProps) {
+    if (this.preImg.src !== nextProps.src) {
+      this.preImg.src = nextProps.src;
+    }
+    StackBlur(this.preImg, this.canvas, nextProps.blurRadius, 600, 190);
+  }
+  render() {
+    var { src, style, ...other } = this.props;
+    return (
+      <div style={style} >
+        <canvas ref='canvas' />
+      </div>
+    )
+  }
+}
+
+class LazyImageWrapper extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleLoaded = this.handleLoaded.bind(this);
+    this.state = {
+      loaded: false
+    }
+  }
+  handleLoaded() {
+    console.log('loaded')
+    this.setState({
+      loaded: true
+    })
+  }
+  render() {
+    var { src, small, width, height, children, className, blurRadius, ...other } = this.props;
+    var styles = {
+      width,
+      height,
+      padding: 0,
+      margin: 0,
+      position: 'absolute',
+      top: 0
+    }
+
+    var wrapperStyles = {
+      paddingBottom: styles.height,
+      position: 'relative'
+    }
+    return (
+      <div style={wrapperStyles}>
+        <FullImage src={src} style={getStyles('FullImage', styles, this.state.loaded)} onLoad={this.handleLoaded} />
+        <LazyImage src={small} style={getStyles('LazyImage', styles, this.state.loaded)} blurRadius={this.props.blurRadius}/>
+      </div>
+    )
+  }
+}
+
+module.exports = LazyImageWrapper
