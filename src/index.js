11ac23b8570012d8d07f65f38535f0468cd9c1fa
Matthias Gattermeier <matthias.gattermeier@nytimes.com>
2016-06-15 17:05:56 -0400

    remove css transitions in favor of dropping absolute positioning

--- src/index.js
+++ src/index.js
@@ -17,32 +17,36 @@ class LazyImageWrapper extends React.Component {
     })
   }
   render() {
-    const { src, small, width, height, blurRadius } = this.props;
+    const { src, low, width, height, blurRadius } = this.props;
+
     const styles = {
       width,
       height,
       padding: 0,
       margin: 0,
-      position: 'absolute',
       top: 0,
     }
 
-    const ease = {
-      WebkitTransition: 'opacity 250ms ease-in-out',
-      MozTransition: 'opacity 250ms ease-in-out',
-      OTransition: 'opacity 250ms ease-in-out',
-      transition: 'opacity 250ms ease-in-out',
-    }
-
-    const wrapperStyles = {
-      paddingBottom: styles.height,
-      position: 'relative'
+    if (!this.props.low) {
+      return <img src={src} style={styles} />
     }
 
     return (
-      <div style={wrapperStyles}>
-        <FullImage src={src} style={getStyles('FullImage', styles, this.state.loaded)} onLoad={this.handleLoaded} />
-        <LazyImage src={small} width={width} height={height} style={getStyles('LazyImage', Object.assign({}, ease, styles), this.state.loaded)} blurRadius={this.props.blurRadius}/>
+      <div>
+        <FullImage
+          src={src}
+          style={getStyles('FullImage', styles, this.state.loaded)}
+          onLoad={this.handleLoaded}
+        />
+        {!this.state.loaded &&
+          <LazyImage
+            src={low}
+            width={width}
+            height={height}
+            style={getStyles('LazyImage', styles, this.state.loaded)}
+            blurRadius={this.props.blurRadius}
+          />
+        }
       </div>
     )
   }
